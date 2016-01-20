a48d473ec5e7a0c710b77c89d04c6672c91710a5
tasilio <tgb@inbox.com>
2016-01-20 12:21:06 +0300

    Update with require 'singleton'.

--- logger.rb
+++ logger.rb
@@ -1,25 +1,15 @@
+require 'singleton'
+
 class Logger
+	
+	include Singleton
+
 	def initialize
 		@f = File.open 'log.txt', 'a'
 	end
 	
-	@@x = Logger.new
-
-	def self.instance # обозначение того, что это класс по паттерну синглтон
-		
-		return @@x
-
-	end
-
-	# class method
-	def self.say_something
-		puts "haha"
-	end
-	# instance method
 	def log_something what  # Если приложение разрастется постоянное открытие и закрытие будет тормозить работу
 		@f.puts what
 	end
-	
-	private_class_method :new # метод new можно вызвать только внутри класса
 
 end
